e17d4b88433285c68912b7a6be8e80cc726fd142
Gift-Chiedozie <chinwe.dozok.blog79620@gmail.com>
2022-12-09 20:25:48 -0800

    More changes

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,26 +6,16 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-
 {
-
 	char alp;
-
+	
 	for (alp = 'a'; alp <= 'z'; alp++)
-
 	{
-
 		if (alp != 'e' && alp != 'q')
-
 			putchar(alp);
-
 	}
-
-
 
 	putchar('\n');
 
-
-
 	return (0);
-
+}
