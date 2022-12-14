1bba26aa839aaeb97ec29e005821dc205e0f4b23
Gift-Chiedozie <chinwe.dozok.blog79620@gmail.com>
2022-12-13 23:27:52 -0800

    Task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the nine times table from zero
+ * Return: 0
+ */
+
+void times_table(void)
+{
+	int x, y, z;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; i < 10; y++)
+		{
+			z = x * y;
+			if (y == 0)
+				_putchar(z + '0');
+			if (y != 0 && z < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(z + '0');
+			}
+			else if (z >= 10)
+			{
+				_putchar(',');
+				_putychar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
