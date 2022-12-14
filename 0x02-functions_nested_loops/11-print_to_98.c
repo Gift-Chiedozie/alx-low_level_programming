f73d8f6b457f34bc357f58b9db7154e81ab87f2a
Gift-Chiedozie <chinwe.dozok.blog79620@gmail.com>
2022-12-14 00:10:22 -0800

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,7 +16,7 @@ void print_to_98(int n)
 			if (n == 98)
 				printf("%d\n", n);
 			else
-				printf("%d,", n);
+				printf("%d, ", n);
 			n = n + 1;
 		}
 	}
@@ -27,7 +27,7 @@ void print_to_98(int n)
 			if (n == 98)
 				printf("%d\n", n);
 			else
-				printf("%d,", n);
+				printf("%d, ", n);
 			n = n - 1;
 		}
 	}
