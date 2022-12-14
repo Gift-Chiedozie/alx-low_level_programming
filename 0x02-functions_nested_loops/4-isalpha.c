8b68bdc26c68cf39defe0f582ed8c0ffbcf21d02
Gift-Chiedozie <chinwe.dozok.blog79620@gmail.com>
2022-12-13 19:40:18 -0800

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,11 +2,11 @@
 
 /**
  * _isalpha - checks for alphabetic characters
- * @c: a character
+ * @c: a character to be checked
  * Return: returns 0 or 1 depending on the condition
  */
 
 int _isalpha(int c)
 {
-	return ((c >= 'a' && c >= 'z') || (c >= 'A' && c >= 'Z'));
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
