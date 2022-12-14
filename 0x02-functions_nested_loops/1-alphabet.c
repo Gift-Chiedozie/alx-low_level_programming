eae6a70fc2bd8731336ba3004a5c12e2ce1524b2
Gift-Chiedozie <chinwe.dozok.blog79620@gmail.com>
2022-12-13 18:19:19 -0800

    Task 0

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Prints the alphabet in lowercase followed by a a new line
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+
+	_putchar('\n');
+}
