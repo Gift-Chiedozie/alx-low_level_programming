ef23cc6b5db0c063d91d66ea314c4c768786b5fe
Gift-Chiedozie <chinwe.dozok.blog79620@gmail.com>
2022-12-16 15:02:04 -0800

    Prints numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,17 @@
 #include "main.h"
 
 /**
- * print_numbers - 
+ * print_numbers -outputs numbers from 0 to 9
+ * Return: Always 0 (Success)
+ */
+
+void print_numbers(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		_putchar(x + '0');
+	}
+	_putchar('\n');
+}
