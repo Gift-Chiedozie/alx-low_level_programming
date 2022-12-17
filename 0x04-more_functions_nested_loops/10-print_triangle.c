0c8101259532c4db7ea37913f74bc036fd6df7eb
Gift-Chiedozie <chinwe.dozok.blog79620@gmail.com>
2022-12-17 06:30:23 -0800

    Prints triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,25 +1,30 @@
 #include "main.h"
 
 /**
- * print_triangle - Prints squares
- * @size: integer parameter
+ * print_triangle - Prints triangle
+ * @size: size of a triangle
  */
 
 void print_triangle(int size)
 {
-	int x, y;
+	int x, y, z;
 
 	if (size <= 0)
 		_putchar('\n');
 	else
 	{
-		for (x = 0; x < size; x++)
+		for (x = 0; x <= (size - 1); x++)
 		{
-			for (y = 0; y < size; y++)
+			for (y = 0; y < (size - 1) - x; y++)
+			{
+				_putchar(' ');
+			}
+			for (z = 0; z <= x; z++)
 			{
 				_putchar(35);
 			}
-			putchar('\n');
+
+			_putchar('\n');
 		}
 	}
 }
