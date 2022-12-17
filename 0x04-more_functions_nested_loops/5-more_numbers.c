447a2ba4abe7fcdb4e640699e118017d289c9d05
Gift-Chiedozie <chinwe.dozok.blog79620@gmail.com>
2022-12-16 16:30:31 -0800

    Numbers constitute the only universal language

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints more numbers
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	int x;
+	int j;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			_putchar(j + '0');
+		}
+	}
+
+	_putchar('\n');
+}
