2f4627bef36a863ee632c90b0c1c7d458fb09633
Gift-Chiedozie <chinwe.dozok.blog79620@gmail.com>
2022-12-20 07:00:18 -0800

    swaps the two integers amongst themselbves

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -5,10 +5,11 @@
  * swap_int - swaps the value of integers
  * @a: parameter swaper
  * @b: parameter swaper as well
- * @temp: a prameter as well
  */
-void swap_int(int *a, int *b, temp)
+void swap_int(int *a, int *b)
 {
+	int temp;
+
 	temp = *a;
 	*a = *b;
 	*b = temp;
