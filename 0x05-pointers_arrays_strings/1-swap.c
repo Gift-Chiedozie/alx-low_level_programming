ff3ee50bb7a9622a793a473863b887f16a3463c4
Gift-Chiedozie <chinwe.dozok.blog79620@gmail.com>
2022-12-20 06:49:53 -0800

    swaps integer values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - swaps the value of integers
+ * @a: parameter swaper
+ * @b: parameter swaper as well
+ * @temp: a prameter as well
+ */
+void swap_int(int *a, int *b, temp)
+{
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
