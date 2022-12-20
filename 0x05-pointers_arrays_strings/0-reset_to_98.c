8b551f38f6a19f40de2513d364fa69af7fa997c8
Gift-Chiedozie <chinwe.dozok.blog79620@gmail.com>
2022-12-20 06:20:50 -0800

    Pointing n to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - resets the integer to from its former value to 98
+ * @n - parameter to reset
+ * return: always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
