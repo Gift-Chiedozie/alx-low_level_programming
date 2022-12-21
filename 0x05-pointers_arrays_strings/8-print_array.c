3148e8c4352228b33d4537ea94e2dd5da9092a90
Gift-Chiedozie <chinwe.dozok.blog79620@gmail.com>
2022-12-21 06:11:37 -0800

    Hot fix

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,8 +13,11 @@ void print_array(int *a, int n)
 
 	for (arr = 0; arr < n; arr++)
 	{
+		printf("%d", a[arr]);
 		if (arr < n + 1)
-			printf("%d, ", a[arr]);
+		{
+			printf(", ");
+		}
 	}
 	printf("\n");
 }
