d8c0c7b92abd13480f765df263b32f34edf5297f
Gift-Chiedozie <chinwe.dozok.blog79620@gmail.com>
2022-12-21 03:01:06 -0800

    reversing the output of a string

---
+++ 0x05-pointers_arrays_strings/5-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - prints a string in reverse
+ * @s: parameter s
+ */
+void rev_string(char *s)
+{
+	char temp;
+	int a, str1, str2;
+
+	str1 = 0;
+	str2 = 0;
+
+	while (s[str1] != '\0')
+		str1++;
+
+	str2 = str1 - 1;
+	for (i = 0; i < str1 / 2; a++)
+	{
+		temp = s[a];
+		s[a] = s[str2];
+		s[str2] = temp;
+		str2 -= 1;
+	}
+	return (str);
+}
