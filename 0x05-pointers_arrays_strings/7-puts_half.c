05060faeaa119c42a9a9e7505924599ffd6c06e9
Gift-Chiedozie <chinwe.dozok.blog79620@gmail.com>
2022-12-21 03:47:10 -0800

    Prints half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - Prints half of a string
+ * @str: a string parameter
+ */
+
+void puts_half(char *str)
+{
+	int len, half_len, i;
+
+	len = strlen(str);
+	half_len = len / 2;
+
+	if (len % 2 == 1)
+	{
+		half_len = (len - 1) / 2;
+	}
+
+	for (i = half_len; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
