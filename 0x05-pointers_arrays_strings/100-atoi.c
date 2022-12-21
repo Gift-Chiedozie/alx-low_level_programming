bd27bdf1a9f69e334552d6105e870985727945a6
Gift-Chiedozie <chinwe.dozok.blog79620@gmail.com>
2022-12-21 12:13:18 -0800

    Another help from Stefan

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,32 @@
+#include <main.h>
+
+/**
+ * _atoi - converts string to integer
+ * @s: points to a character string
+ * Return: void
+ */
+
+int _atoi(char *s)
+{
+	int a;
+	unsigned int b;
+	char *temp;
+
+	temp = s;
+	b = 0;
+	a = 1;
+	while (*temp != '\0' && (*temp < '0' || *temp > '9'))
+	{
+		if (*temp == '-')
+			a *= -1;
+		temp++;
+	}
+	if (*temp != '\0')
+	{
+		do {
+			b = b * 10 + (*temp - '0');
+			temp++;
+		} while (*temp >= '0' && *temp <= '9');
+	}
+	return (b * a);
+}
