0927d7c10b7b8c291ad0667b04e87efeb6cf60e5
Gift-Chiedozie <chinwe.dozok.blog79620@gmail.com>
2022-12-21 05:41:48 -0800

    Prints an array of integer

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints array of an element of integers
+ * @a: parameter 1
+ * @n: parameter 2
+ */
+
+void print_array(int *a, int n)
+{
+	int arr;
+
+	for (arr = 0; arr < n; arr++)
+	{
+		if (a < n - 1)
+			printf("%d, ", a[arr]);
+	}
+	printf("\n");
+}
