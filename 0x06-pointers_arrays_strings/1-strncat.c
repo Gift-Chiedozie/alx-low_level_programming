882acacc9af8d5c62bffbe5a8262b0a6b1008acc
Gift-Chiedozie <chinwe.dozok.blog79620@gmail.com>
2022-12-22 08:19:02 -0800

    Concatenation of a string

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,17 +5,18 @@
  * *_strcat - concatenates the value of a astring
  * @dest: string to be appended
  * @src: string to be concatenated
- * Return: returns the pointer to @dest
+ * @n: integer parameter to compare with
+ * Return: returns a new string
  */
 
-char *_strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src, int n)
 {
 	int index = 0, dest_len = 0;
 
 	while (dest[index++])
 		dest_len++;
 
-	for (index = 0; src[index]; index++)
+	for (index = 0; src[index] && index < n; index++)
 		dest[dest_len++] = src[index];
 	return (dest);
 }
