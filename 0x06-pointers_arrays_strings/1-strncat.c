c2f382b84d1aa7cff4067d7e14a1855a2d215f2a
Gift-Chiedozie <chinwe.dozok.blog79620@gmail.com>
2022-12-22 08:08:16 -0800

    Concatenates a string

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates the value of a astring
+ *  @dest: string to be appended
+ *  @src: string to be concatenated
+ *  Return: returns the pointer to @dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+	return (dest);
+}
