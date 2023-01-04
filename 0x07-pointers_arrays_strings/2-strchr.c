ca19e9c108e5e1129e4a59bb5416df0c1351c319
Gift-Chiedozie <chinwe.dozok.blog79620@gmail.com>
2023-01-04 07:05:53 -0800

    Pointing to first occurence of a character

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - String character
+ * @s: another string character
+ * @c: and another string character
+ *
+ * Return: Returns a pointer to the first occurrence of the character
+ */
+
+char *_strchr(char *s, char c)
+{
+	int index = 0, b;
+
+	while (s[index])
+		index++;
+	for (b = 0; b < index; b++)
+	{
+		if (c == s[index])
+			s += b;
+		return (s);
+	}
+	return ('\0');
+}
