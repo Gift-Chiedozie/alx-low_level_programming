a2eb4fff614eb81850a8bbe541f59517f497a3aa
Gift-Chiedozie <chinwe.dozok.blog79620@gmail.com>
2023-01-30 04:31:06 -0800

    The main header file was previously omitted

--- 0x12-singly_linked_lists/main.h
+++ 0x12-singly_linked_lists/main.h
@@ -1,1 +1,11 @@
+#ifndef _SINGLY_LINKED_LISTS_H
+#define _SINGLY_LINKED_LISTS_H
 
+void first(void)__attrtibute__((constructor));
+void free_list(list_t *head);
+list_t *add_node_end(list_t **head, const char *str);
+list_t *add_node(list_t **head, const char *str);
+size_t list_len(const list_t *h);
+size_t print_List(const list_t *h);
+
+#endif
