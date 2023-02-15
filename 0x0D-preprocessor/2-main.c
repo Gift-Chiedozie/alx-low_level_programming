3a3f42580ea9b29a2e6d14a36f27a3f12ffce575
Gift-Chiedozie <chinwe.dozok.blog79620@gmail.com>
2023-02-15 06:14:36 -0800

    Task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - writes a function that prints the name of current file
+ * void: gives no real value
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("The file name of this program is: %s\n", __FILE__);
+	return (0);
+}
