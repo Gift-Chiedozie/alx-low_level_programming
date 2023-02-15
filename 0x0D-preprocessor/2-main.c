cc04b69b7a66986ae1a2d676a35fb19017b75e5f
Gift-Chiedozie <chinwe.dozok.blog79620@gmail.com>
2023-02-15 06:51:47 -0800

    Task 2 once again corrected

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - writes a function that prints the name of current file
- *
- * Return: Always 0 (success)
+ * main - writes a function that prints the name of current file a program was compiled frim
+ * Return: Always 0
  */
 
 int main(void)
 {
-	printf("The file name of this program is: %s\n", __FILE__);
+	printf("The name of this program file is: %s\n", __FILE__);
+	printf("\n");
 	return (0);
 }
