338f1fdcf3f8f0ef0daa764cafb0c462e2dcc66c
Gift-Chiedozie <chinwe.dozok.blog79620@gmail.com>
2023-02-16 21:12:50 -0800

    Beauty is variable, ugliness is constant

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <sdarg.h>
+
+/**
+ * sum_them_all - write a function that prints the sum of all its parameters
+ * @n: number of parameters passed to the function
+ * @...: variable number of parameters that can be passed to calculae sum
+ *
+ * Return: if n == 0 - 0.
+ *	otherwise - return the sum of all parameters
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i, sum = 0;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+		sum = sum + va_arg(ap, int);
+
+	va_end(ap);
+
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,18 @@
+#variadic funtions
+
+=====================
+#Task 0
+writes a function that prints or returns the sum if all the parameters
+
+=====================
+#Task 1
+writes a function that prints numbers followed by a new line
+
+=====================
+#Task 2
+writes a function that prints string , alongside a new line
+/*without usung the printf function*/
+
+=====================
+#Task 3
+Writes a function that prints anything at all.
