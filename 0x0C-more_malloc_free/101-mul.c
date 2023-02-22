2eb34cbede55f3fdb4184c0c98c86df267e88c9c
Gift-Chiedozie <chinwe.dozok.blog79620@gmail.com>
2023-02-21 20:08:33 -0800

    Task 2 advanced

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -58,7 +58,7 @@ void errors(void)
  *
  * Return: always 0 (Success)
  */
-int main(int argc, char *argv[])		
+int main(int argc, char *argv[])
 {
 	char *s1, *s2;
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
@@ -74,7 +74,7 @@ int main(int argc, char *argv[])
 		return (1);
 	for (i = 0; i <= len1 + len2; i++)
 		result[i] = 0;
-	for (len1 = len1 - 1; len1 >= 0; len1--)	
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
 		digit1 = s1[len1] - '0';
 		carry = 0;
@@ -86,18 +86,18 @@ int main(int argc, char *argv[])
 			carry /= 10;
 		}
 		if (carry > 0)
-			result[len1 + len2 + 1] += carry;	
+			result[len1 + len2 + 1] += carry;
 	}
-	for (i = 0; i < len - 1; i++)	
+	for (i = 0; i < len - 1; i++)
 	{
 		if (result[i])
 			a = 1;
 		if (a)
-			_putchar(result[i] + '0');	
-	}	
+			_putchar(result[i] + '0');
+	}
 	if (!a)
 		_putchar('0');
 	_putchar('\n');
 	free(result);
-	return (0);		
+	return (0);
 }
