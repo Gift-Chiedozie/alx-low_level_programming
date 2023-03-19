71655d8080447f60bec4d0705bcc03dd8d5f0157
Gift-Chiedozie <chinwe.dozok.blog79620@gmail.com>
2023-03-18 18:35:26 -0700

    Print lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -3,31 +3,32 @@
 #include "lists.h"
 
 /**
- * size_t print_dlistint - writes a function that prints the number of elements
+ * print_dlistint - writes a function that prints the number of elements
  * in a dlistint_t list
- * @*h: the parameter useed to represent the head node
+ * @h: the parameter useed to represent the head node
  *
  * Return: number of nodes present in list.
  */
 
-struct node* print_dlistint(struct node *h)
+struct node *print_dlistint(struct node *h)
 {
 	int node, data, i;
+
 	printf("Enter the number of nodes: ");
-	scanf("%d", &node); 
+	scanf("%d", &node);
 
 	if (node == 0)
-		return h;
+		return (h);
 
 	printf("Enter element for the node 1: ");
 	scanf("%d", &data);
 	h = dlistint_s(h, data);
 
-	for(i = 1; i < node; i++)
+	for (i = 1; i < node; i++)
 	{
-		printf("Enter the element for the node %d: ", i+1)
+		printf("Enter the element for the node %d: ", i + 1)
 		scanf("%d", &data);
 		h = dlistint_t(h, data);
 	}
-	return h;
+	return (h);
 }
