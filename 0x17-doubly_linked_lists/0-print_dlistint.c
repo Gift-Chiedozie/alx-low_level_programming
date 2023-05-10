02f5051627f4b1817c60b8982a04bbd6ae55bd06
Gift-Chiedozie <chinwe.dozok.blog79620@gmail.com>
2023-05-09 21:55:02 -0700

    Poppy

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -3,13 +3,13 @@
 #include "lists.h"
 
 /**
- * size_t print_dlistint - writes a function that prints all the element of a list
+ * print_dlistint - writes a function that prints all the element of a list
  * @h: parameter that represents the head node
  *
  * Return: the total number of nodes
  */
 
-struct size_t print_dlistint(const dlistint_t *h)
+struct print_dlistint(const dlistint_t *h)
 {
 	dlistint_t *ptr;
 
@@ -17,6 +17,6 @@ struct size_t print_dlistint(const dlistint_t *h)
 		printf("List is empty");
 	else
 		for(ptr = h; ptr != NULL; ptr = ptr->next)
-			printf("%d\n", ptr->data);
-	return(h);
+			printf("%d\n", ptr->n);
+	return(void);
 }
