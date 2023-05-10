00387f56f39246b9bd863f01e674f5c5acbf1555
Gift-Chiedozie <chinwe.dozok.blog79620@gmail.com>
2023-05-10 02:11:28 -0700

    Still making corrections

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,13 +1,23 @@
 #include <stdlib.h>
-#include "lists.h"  /*Replace with the actual header file containing the definition of dlistint_t*/
+#include "lists.h"
+/*Replace with the actual header file containing the definition of dlistint_t*/
+
+/**
+ * *add_dnodeint - prints a new node at the
+ * beginning of a list dlistint_t
+ * @head: the head node that check whether list is empty
+ * @n: mumber used to traverse the list
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node = malloc(sizeof(dlistint_t)); /*Create a new node*/
+
 	if (new_node == NULL)
 	{
-		return NULL;
-
+		return (NULL);
 	}
 
 	new_node->n = n; /*Set the value of the new node*/
@@ -15,7 +25,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	if (*head != NULL)
 	{
-		(*head)->prev = new_node; /*If the list is not empty, update the previous pointer of the current head*/
+		(*head)->prev = new_node;
+		/**
+		 * If the list is not empty,
+		 * update the previous pointer of the current head
+		 */
 	}
 	/*Set the next pointer of the new node to the current head*/
 	new_node->next = *head;
@@ -23,5 +37,5 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	/*Update the head pointer to point to the new node*/
 	*head = new_node;
 
-	return new_node;
+	return (new_node);
 }
